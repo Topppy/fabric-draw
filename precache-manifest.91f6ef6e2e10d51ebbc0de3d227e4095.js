11146ac8cfbc88e0ffacf51549cd05dea0b32bfc
Topppy <taoyiming2010@126.com>
2019-08-19 14:40:44 +0800

    Updates

---
+++ precache-manifest.91f6ef6e2e10d51ebbc0de3d227e4095.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98245ed8960a8dea31e128141143f3de",
+    "url": "/fabric-draw/index.html"
+  },
+  {
+    "revision": "715b47449eed09cc1548",
+    "url": "/fabric-draw/static/css/main.79af3333.chunk.css"
+  },
+  {
+    "revision": "a1fe00afebf8598cf69c",
+    "url": "/fabric-draw/static/js/2.ef01c3ee.chunk.js"
+  },
+  {
+    "revision": "715b47449eed09cc1548",
+    "url": "/fabric-draw/static/js/main.18657e14.chunk.js"
+  },
+  {
+    "revision": "2bbf3eb5029c09924d90",
+    "url": "/fabric-draw/static/js/runtime~main.0f179c1b.js"
+  }
+]);
